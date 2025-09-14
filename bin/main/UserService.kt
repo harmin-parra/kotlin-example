e83254e93c8f44a9ca807ac4f05f8889b5dae4cd
Harmin Parra Rueda <harmin.rueda@gmail.com>
2025-09-14 12:40:48 +0200

    update

--- bin/main/UserService.kt
+++
@@ -1,6 +0,0 @@
-class UserService(private val repository: UserRepository) {
-    fun greetUser(id: Int): String {
-        val name = repository.findNameById(id)
-        return "Hello, $name!"
-    }
-}
